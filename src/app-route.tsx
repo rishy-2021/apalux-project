c1762a02997e9dd936940a90f7f78355f97aaab8
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-01-07 02:01:55 +0530

    change route order

--- src/app-route.tsx
+++ src/app-route.tsx
@@ -1,20 +1,20 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-import { suspend } from 'suspend-react';
-import { TopBar } from './components/top-bar';
-import { DashBoard } from './pages/dashboard';
-import { useStores } from './utils/use-stores';
-import { SignUp } from './pages/auth/sign-up';
-import { SignIn } from './pages/auth/sign-in';
-import VerifyEmail from './pages/auth/verify-email';
+import { suspend } from "suspend-react";
+import { TopBar } from "./components/top-bar";
+import { DashBoard } from "./pages/dashboard";
+import { useStores } from "./utils/use-stores";
+import { SignUp } from "./pages/auth/sign-up";
+import { SignIn } from "./pages/auth/sign-in";
+import VerifyEmail from "./pages/auth/verify-email";
 
 const AppRoute: React.FC = observer(() => {
   const location = useLocation();
   const {
     userStore: {
       auth: { isAuthenticated },
-    }
+    },
   } = useStores();
 
   suspend(async () => {
@@ -24,21 +24,17 @@ const AppRoute: React.FC = observer(() => {
   if (!isAuthenticated) {
     return (
       <>
-        {!location.pathname.toLowerCase().includes("verify-email") && <TopBar />}
+        {!location.pathname.toLowerCase().includes("verify-email") && (
+          <TopBar />
+        )}
         <Routes>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
           <Route
-            path='signin'
-            element={<SignIn />}
-          />
-          <Route
-            path='signup'
-            element={<SignUp />}
-          />
-          <Route
-            path='verify-email/:id/:accessToken'
+            path="verify-email/:id/:accessToken"
             element={<VerifyEmail />}
           />
-          <Route path='*' element={<Navigate to='signin' replace />} />
+          <Route path="*" element={<Navigate to="signup" replace />} />
         </Routes>
       </>
     );
@@ -48,13 +44,11 @@ const AppRoute: React.FC = observer(() => {
     <>
       <TopBar />
       <Routes>
-        <Route path='dashboard' element={<DashBoard />} />
-        <Route path='*' element={<Navigate to='dashboard' replace />} />
+        <Route path="dashboard" element={<DashBoard />} />
+        <Route path="*" element={<Navigate to="dashboard" replace />} />
       </Routes>
     </>
-
-  )
+  );
 });
 
 export default AppRoute;
-
